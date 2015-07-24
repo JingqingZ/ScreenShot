63e05bd0e36adae5c058a5f801068b45e91f7034
unknown <zhangjqsmiling@gmail.com>
2015-07-24 11:07:46 +0100

    first version

---
+++ ScreenShot/ScreenShot/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ScreenShot
+{
+    class ScreenShot
+    {
+        public static void CaptureScreen(int seconds, int limit, string destDir)
+        {
+            int id = 0;
+            Directory.CreateDirectory(destDir);
+            String[] filePaths = Directory.GetFiles(destDir);
+            foreach (String filePath in filePaths)
+                File.Delete(filePath);
+
+            while (id < limit)
+            {
+                Rectangle bounds = Screen.GetBounds(Point.Empty);
+                using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+                {
+                    using (Graphics g = Graphics.FromImage(bitmap))
+                    {
+                        g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                    }
+                    bitmap.Save(destDir + "\\" + id + ".bmp", ImageFormat.Bmp);
+                    id ++;
+                    Thread.Sleep(seconds * 1000);
+                }
+
+            } 
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(args[1]);
+            ScreenShot.CaptureScreen(Convert.ToInt32(args[0]), Convert.ToInt32(args[1]), args[2]);
+        }
+    }
+}
